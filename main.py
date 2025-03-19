0f1187ae2f126adc8bd09c159fbd5d956b2c5341
uday kiran thigala <117595327+udaykiranthigala@users.noreply.github.com>
2025-03-19 22:08:52 +0530

    Add files via upload
    Deployment task-3

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.image("C:/Users/LENOVO/OneDrive/Desktop/PYTHON_TUTORIAL/streamlit/scifortechnologies_logo.png",
+         use_column_width=True)
+
+st.markdown("""<h1 style='text-align: center; font-size: 50px;'>Meta Scifor Mega Hiring</h1>""",
+            unsafe_allow_html=True)
+
+is_fresher = st.radio("Are you a fresher?", ("Yes", "No"))
+
+if is_fresher == "Yes":
+    st.write("Answer the below question correctly to get selected!")
+
+    answer = st.slider("What is the decimal number equivalent to 101010?", min_value=1, max_value=100, value=50)
+    correct_answer = 42
+
+    if st.button("Submit Answer"):
+        if answer == correct_answer:
+            st.success("Congratulations! You are selected!")
+        else:
+            st.error("Sorry, wrong answer. Try again!")
+
